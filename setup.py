f6293608eac64af3a525a1074a82df8746e1f977
theweaklink <cavelius@protonmail.com>
2025-06-12 22:33:30 +0200

    update to version 1.2.0 to match onecode version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def get_long_desc():
 def setup_package():
     setup(
         name="onecode-pycg",
-        version="0.0.9",
+        version="1.2.0",
         description="Practical Python Call Graphs",
         long_description=get_long_desc(),
         long_description_content_type="text/markdown",
